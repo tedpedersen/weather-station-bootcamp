2e7e0ae3341749eab6e7ec4d507621ad8b885e97
Ted Pedersen <tedpedersen@me.com>
2020-06-20 16:03:38 -0700

    fetcing api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,18 @@ var getWeather = function(city) {
     // format the api url api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=fabf22bfb54443507c45e344ea64f584";
   
-    // make a request to the url
+    //make a request to the url
     fetch(apiUrl).then(function(response) {
+
       response.json().then(function(data) {
         console.log(data);
       });
+
     });
+
+    
   };
-  
+
   var userFormEl = document.querySelector("#searchForm");
   var nameInputEl = document.querySelector("#searchInput");
   
@@ -18,11 +22,14 @@ var getWeather = function(city) {
     // get value from input element
     var cityname = nameInputEl.value.trim();
   
-    if (username) {
+    if (cityname) {
       getWeather(cityname);
       nameInputEl.value = "";
-    } else {
-      alert("Please enter a city name.");
+      var searchHistory = `<li class="list-group-item">${cityname}</li>`;
+      $("#historyList").append(searchHistory);
+    }
+     else {
+      alert("Please enter a valid city name.");
     }
     console.log(event);
   };
